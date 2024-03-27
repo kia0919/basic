d957a7c977f1823f60711916ba56352bd9979c3c
kia0919 <john0256@naver.com>
2024-03-27 15:25:36 +0900

    2024.03.27

--- src/main/java/com/umsuhyun/basic/controller/StudentController.java
+++ src/main/java/com/umsuhyun/basic/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.umsuhyun.basic.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-    
+
+    // CREATE
     @PostMapping("/")
     public ResponseEntity<?> postStudent () {       // 제너릭에는 데이터 타입이 전달되어야 함 
         return null;
     }     
+
+    // UPDATE
+    @PatchMapping("/")
+    public ResponseEntity<?> patchStudent () {
+        return null;
+    }
+
+    // DELETE
+    @DeleteMapping("/{studentNumber}")
+    public ResponseEntity<?> deleteStudent(
+        @PathVariable("studentNumber") Integer studentNumber
+    ) {
+        return null;
+    }
 
 }

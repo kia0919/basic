68d94707ae8c928ef0900a80b74f642f3df24953
kia0919 <john0256@naver.com>
2024-03-27 16:28:48 +0900

    2024.03.27

--- src/main/java/com/umsuhyun/basic/controller/StudentController.java
+++ src/main/java/com/umsuhyun/basic/controller/StudentController.java
@@ -5,16 +5,29 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.umsuhyun.basic.dto.request.student.PostStudentRequestDto;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/student")
+@RequiredArgsConstructor
 public class StudentController {
+
+
+    private final com.umsuhyun.basic.service.studentService studentService;
 
     // CREATE
     @PostMapping("/")
-    public ResponseEntity<?> postStudent () {       // 제너릭에는 데이터 타입이 전달되어야 함 
+    public ResponseEntity<String> postStudent (  // 제너릭에는 데이터 타입이 전달되어야 함 
+        @RequestBody @Valid PostStudentRequestDto requestBody
+    ) {
+        ResponseEntity<String> response = studentService.postStudent(requestBody);
         return null;
     }     
 
---
+++ src/main/java/com/umsuhyun/basic/service/implement/StudentServiceImplement.java
@@ -0,0 +1,30 @@
+package com.umsuhyun.basic.service.implement;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.umsuhyun.basic.Entity.StudentEntity;
+import com.umsuhyun.basic.dto.request.student.PostStudentRequestDto;
+import com.umsuhyun.basic.repository.StudentRepository;
+import com.umsuhyun.basic.service.studentService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StudentServiceImplement implements studentService {
+    
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<String>
+    postStudent(PostStudentRequestDto dto) {
+
+        StudentEntity studentEntity = new StudentEntity(dto);
+        studentRepository.save(studentEntity);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("성공!");
+    }
+
+}

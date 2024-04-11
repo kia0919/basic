c68c6202a4b7f2df9558b50cce871bfe4d845e4f
kia0919 <john0256@naver.com>
2024-04-11 16:44:07 +0900

    2024.04.11-16:50

--- src/main/java/com/umsuhyun/basic/service/implement/BasicServiceImplement.java
+++ src/main/java/com/umsuhyun/basic/service/implement/BasicServiceImplement.java
@@ -2,7 +2,10 @@ package com.umsuhyun.basic.service.implement;
 
 import org.springframework.stereotype.Service;
 
+import com.umsuhyun.basic.provider.JwtProvider;
 import com.umsuhyun.basic.service.BasicService;
+
+import lombok.RequiredArgsConstructor;
 
 //! Service 레이어 :
 // - 실제 비즈니스 로직(연산)을 실행하는 영역
@@ -13,7 +16,11 @@ import com.umsuhyun.basic.service.BasicService;
 // @Component : 해당 클래스를 Spring Bean으로 등록하는 어노테이션
 // Spring Bean : 제어의 역전을 통해서 의존성 주입시 해당 클래스의 인스턴스를 Spring Framework가 제어하는 요소
 @Service
+@RequiredArgsConstructor
 public class BasicServiceImplement implements BasicService {
+
+    private final JwtProvider jwtProvider;
+
 
     @Override
     public String getHello() {
@@ -27,7 +34,7 @@ public class BasicServiceImplement implements BasicService {
 
     @Override
     public String getJwt(String priciple) {
-        
+        return jwtProvider.create(priciple);
     }
     
 }

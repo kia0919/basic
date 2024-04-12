7ffa5c1151a87754f40954ea3196ff3de0ae24d7
kia0919 <john0256@naver.com>
2024-04-12 16:49:39 +0900

    2024.04.12 16:50

---
+++ src/main/java/com/umsuhyun/basic/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.umsuhyun.basic.filter;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// OncePerRequestFilter:
+// - 해당 클래스를 필터 클래스로 지정하는 추상클래스
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+    
+}

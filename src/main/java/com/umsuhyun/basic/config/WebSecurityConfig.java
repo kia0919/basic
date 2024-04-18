11327804a203450a10f4ad1f568a918bc5191b19
kia0919 <john0256@naver.com>
2024-04-18 15:52:08 +0900

    2024.04.18 15:50

--- src/main/java/com/umsuhyun/basic/config/WebSecurityConfig.java
+++ src/main/java/com/umsuhyun/basic/config/WebSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.umsuhyun.basic.filter.JwtAuthenticationFilter;
+import com.umsuhyun.basic.provider.OAuth2SuccessHandler;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,10 +30,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
-// @Configurable : 
+//! @Configurable : 
 // - 생성자나 '메서드'가 호출시에 Spring bean 등록을 자동화(제어 역전)하는 어노테이션 
 @Configurable
-// @EnableWebSecurity : 
+//! @EnableWebSecurity : 
 // - Web Securirty 설정을 지원하도록 하는 어노테이션
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -40,32 +41,33 @@ public class WebSecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final OAuth2UserService oAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
-    // @Bean : 
+    //! @Bean : 
     // - Spring bean으로 등록하는 어노테이션
     // - @Component를 사용하지 못할 때 사용
     // - @Autowired의 목적이 아닐때 사용
     @Bean
     protected SecurityFilterChain configure(HttpSecurity security) throws Exception {
-        // class::method :
+        //! class::method :
         // - 메소드 참조, 특정 클래스의 메서드를 참조할 때 사용
         // - 일반적으로 매개변수로 메서드를 전달할 때 사용됨
         security
-        // basic authentication 미사용 지정
+        //* */ basic authentication 미사용 지정
         .httpBasic(HttpBasicConfigurer::disable)
-        // session : 
+        //! session : 
         // - 웹 애플리케이션에서 사용자의 대한 정보 및 상태를 유지하기 위한 기술
         // - 서버측에서 사용자 정보 및 상태를 저장하는 방법
         // - REST API 서버에서는 사용자 정보 및 상태를 클라이언트가 유지하기 때문에 session을 생성하지 않음
-        // cookie :
+        //! cookie :
         // - 웹 애플리케이션에서 사용자의 대한 정보 및 상태를 유지하기 위한 기술
         // - 클라이언트측에서 사용자 정보 및 상태를 저장하는 방법
-        // session과 cookie의 차이:
+        //! session과 cookie의 차이:
         // - 저장위치 : cookie는 클라이언트, session은 서버
         // - 보안 : session이 보안 수준이 높음
         // - 수명 : cookie는 지정한 기간동안 지속적으로 유지, session은 연결이 끊기면 파기됨
         // - 용도 : cookie에는 간단한 데이터(id, token)를 저장, session에는 민감한 데이터(개인정보)를 저장
-        // cache:
+        //! cache:
         // - 데이터나 값을 미리 복사해두고 저장하는 임시 공간
         // - 사용자의 접근을 조금 더 빠르게 할 수 있도록 함
         // - 시스템 성능 향상
@@ -73,35 +75,37 @@ public class WebSecurityConfig {
         // - 소프웨어 캐시 : web cache, database cache
         // - 네트워크 캐시 : CDN
         .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        // CSRF (Cross-Site Request Forgery): 
+        //! CSRF (Cross-Site Request Forgery): 
         // - 클라이언트(사용자)가 자신의 의도와는 무관한 공격 행위를 하는 것
-        // SQL Injection : 
+        //! SQL Injection : 
         // - 공격자가 데이터베이스의 쿼리문을 직접 조작하여 데이터를 탈취하는 공격
-        // XSS (Cross-Site Scripting) :
+        //! XSS (Cross-Site Scripting) :
         // - 공격자가 웹 브라우저에 악성 스크립트를 작성하여 실행시키는 공격
         .csrf(CsrfConfigurer::disable)
-        // Spring Security 사용 이후에는 CORS 정책을 Security Filter Chain에 등록
+        //* */ Spring Security 사용 이후에는 CORS 정책을 Security Filter Chain에 등록
         .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        // 요청 URL의 패턴에 따라 리소스 접근 허용 범위를 지정
+        //! 요청 URL의 패턴에 따라 리소스 접근 허용 범위를 지정
         // - 인증되지 않은 사용자도 접근을 허용
         // - 인증된 사용자 중 특정 권한을 가지고 있는 사용자만 접근을 허용
         // - 인증된 사용자는 모두 접근을 허용
         .authorizeHttpRequests(request -> request
-            .requestMatchers("/oauth2/**").permitAll()
-            // 특정 URL 패턴에 대한 요청은 인증되지 않은 사용자도 접근을 허용
+            .requestMatchers("/oauth2/**", "/").permitAll()
+            //* */ 특정 URL 패턴에 대한 요청은 인증되지 않은 사용자도 접근을 허용
             .requestMatchers(HttpMethod.GET, "/auth/**").permitAll()
-            // 특정 URL 패턴에 대한 요청은 지정한 권한을 가지고 있는 사용자만 접근을 허용
+            //* */ 특정 URL 패턴에 대한 요청은 지정한 권한을 가지고 있는 사용자만 접근을 허용
             // .requestMatchers("/student", "/student/**").hasRole("STUDENT")
             .requestMatchers("/student", "/student/**").permitAll()
-            // 인증된 사용자는 모두 접근을 허용
+            //* */ 인증된 사용자는 모두 접근을 허용
             .anyRequest().authenticated()
         )
         .oauth2Login(oauth2 -> oauth2
-            // OAuth 인증 서버에서 redirection 하는 URL 지정
+            //* */ OAuth 인증 서버에서 redirection 하는 URL 지정
             .redirectionEndpoint(endpoint -> 
             endpoint.baseUri("/oauth2/callback/*"))
             // OAuth 인증 서버에서 인증 절차가 끝난 후 사용자에 대한 정보를 처리하는 객체를 지정
             .userInfoEndpoint(endpoint -> endpoint.userService(oAuth2UserService))
+            // OAuth 인증 요청 성공시 처리하는 객체를 지정
+            .successHandler(oAuth2SuccessHandler)
         )
         // 인증 과정 중에 발생한 예외 처리
         .exceptionHandling(exceptionHandling -> exceptionHandling
@@ -131,7 +135,7 @@ public class WebSecurityConfig {
 
 }
 
-// 인증 실패 처리를 위한 커스텀 예외 처리 (AuthenticationEntryPoint 인터페이스 구현)
+//* */ 인증 실패 처리를 위한 커스텀 예외 처리 (AuthenticationEntryPoint 인터페이스 구현)
 class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
--- src/main/java/com/umsuhyun/basic/entity/CustomOAuth2User.java
+++ src/main/java/com/umsuhyun/basic/entity/CustomOAuth2User.java
@@ -8,8 +8,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import lombok.AllArgsConstructor;
 
-// OAuth2.0 인증에 사용되는 유저에 대한 정보를 담는 인스턴스를 생성하는 클래스
-// OAuth2User 인터페이스를 구현하여 작성해야함
+//* */ OAuth2.0 인증에 사용되는 유저에 대한 정보를 담는 인스턴스를 생성하는 클래스
+//* */ OAuth2User 인터페이스를 구현하여 작성해야함
 @AllArgsConstructor
 public class CustomOAuth2User implements OAuth2User {
 
